1ac5521a667bd84aadb7260e8fc0f0d06bb76921
kgh <vot768@naver.com>
2021-10-17 21:24:38 +0900

    페이지 이동 연습

--- src/PageMove.java
+++ src/PageMove.java
@@ -1,9 +1,12 @@
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.BoxLayout;
-import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class PageMove {
 
@@ -42,6 +45,41 @@ public class PageMove {
 		frame.setLocationRelativeTo(null);	//모니터 중앙에 띄우기
 		frame.setResizable(false);	//창 크기 조절 불가
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		
+		JPanel endpage = new JPanel();
+		frame.getContentPane().add(endpage);
+		endpage.setBounds(0, 0, 794, 565);
+		endpage.setLayout(null);
+		
+		JButton lastbtn = new JButton("LAST");
+		lastbtn.setBounds(0, 0, 97, 23);
+		lastbtn.setFont(new Font("Tahoma", Font.BOLD, 15));
+		endpage.add(lastbtn);
+		
+		JPanel startpage = new JPanel();
+		startpage.setBounds(0, 0, 794, 565);
+		frame.getContentPane().add(startpage, BorderLayout.NORTH);
+		
+		JButton nextbtn = new JButton("NEXT");
+		startpage.add(nextbtn);
+		
+		endpage.setVisible(false);
+		
+		lastbtn.addActionListener(new ActionListener() {
 
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				startpage.setVisible(true);
+				endpage.setVisible(false);
+			}
+		});
+		nextbtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				startpage.setVisible(false);
+				endpage.setVisible(true);
+			}
+		});
+	}
 }
